bec2bce090c139289670b6f683448166b8f2d7cb
Łukasz <36705519+Qkyyy@users.noreply.github.com>
2018-10-18 18:10:27 +0200

    added object that is destructible, added force applied to parts of cracked object

---
+++ Assets/AddForceToClone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceToClone : MonoBehaviour {
+
+    public float power = 1f;
+
+    Vector3 direction;
+
+    Rigidbody[] piecesOfObj;
+
+    void Start ()
+    {
+        direction = -Camera.main.transform.position;
+        piecesOfObj = new Rigidbody[transform.childCount];
+
+        for (int i=0;i<transform.childCount;i++)
+        {
+            piecesOfObj[i] = transform.GetChild(i).GetComponent<Rigidbody>();
+        }
+
+        foreach (Rigidbody element in piecesOfObj)
+        {
+            element.AddForce(direction * power, ForceMode.Impulse);
+        }
+	}
+	
+}
---
+++ Assets/DestroyObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+    public GameObject crackedObject;
+
+
+    private void OnMouseDown()
+    {
+        Instantiate(crackedObject, transform.position, transform.rotation);
+        Destroy(transform.gameObject);
+    }
+}
