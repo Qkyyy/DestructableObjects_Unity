5468ecf6ccd1ecabb87ae93a6aac02978a755905
Łukasz <36705519+Qkyyy@users.noreply.github.com>
2018-10-18 19:06:10 +0200

    added comments to scripts

--- Assets/_Scripts/AddForceToClone.cs
+++ Assets/_Scripts/AddForceToClone.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class AddForceToClone : MonoBehaviour {
 
-    public float power = 1f;
+    public float power = 5f;                //amount of power added to the parts of destructed objects
 
-    Vector3 direction;
+    Vector3 direction;                      //direction in which parts of destructed objects will go
 
-    Rigidbody[] piecesOfObj;
+    Rigidbody[] piecesOfObj;                //Array of Rigidbodies of the smaller parts of the object
 
     void Start ()
     {
-        direction = -Camera.main.transform.position;
-        piecesOfObj = new Rigidbody[transform.childCount];
+        direction = -Camera.main.transform.position;        //direction is the camera.position inverse
 
+        piecesOfObj = new Rigidbody[transform.childCount]; // we set the array size to match the count of smaller parts
+
+
+        //we attribute all of the parts to the array
         for (int i=0;i<transform.childCount;i++)
         {
             piecesOfObj[i] = transform.GetChild(i).GetComponent<Rigidbody>();
         }
 
+
+        //for each part in the array, we add force to it
         foreach (Rigidbody element in piecesOfObj)
         {
             element.AddForce(direction * power, ForceMode.Impulse);
--- Assets/_Scripts/DestroyObject.cs
+++ Assets/_Scripts/DestroyObject.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class DestroyObject : MonoBehaviour {
 
-    public GameObject crackedObject;
+    public GameObject crackedObject;            //GameObject of the CrackedObject version 
 
 
+
+    //Mouse click on the object
     private void OnMouseDown()
     {
-        Instantiate(crackedObject, transform.position, transform.rotation);
-        Destroy(transform.gameObject);
+        Instantiate(crackedObject, transform.position, transform.rotation);         //We replace original object with the cracked version
+        Destroy(transform.gameObject);                                              //Destroy the original object
     }
 }
